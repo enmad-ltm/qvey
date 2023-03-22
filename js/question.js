716107656e1e93b687897453a3b8f9dd9292fcfd
skyL <crowzodiac@gmail.com>
2023-03-22 11:36:19 +0900

    이벤트 신규 생성 - 개인정보취합/설문조사 탭 모두 객관식, 주관식 문항 미리보기 가능하도록 기능추가

--- js/question.js
+++ js/question.js
@@ -2,53 +2,51 @@ function chkChg (){
     console.log("this: ", this);
 }
 var qType = '';
+var crtTab = 'privacy';
 //var sfaType = '';
 $('select[name=slWrapperSelect]').change(function (){
   qType = $(this).val();
   console.log('qType:', qType);
   if (qType == 'multi-choice') {
-    $('#privacyMCA').addClass('d-block');
-    $('#prReqBtn').addClass('d-block');
+    $('#privacyMCA, #prReqBtn').addClass('d-block');
     $('#privacySFA').removeClass('d-block');
     $('#prReqBtn').addClass('mg-l-10');
   } else if (qType == 'short-form') {
     $('#privacyMCA').removeClass('d-block');
-    $('#privacySFA').addClass('d-block');
-    $('#prReqBtn').addClass('d-block');
+    $('#privacySFA, #prReqBtn').addClass('d-block');
     $('#prReqBtn').addClass('mg-l-10');
     $('#privacyMCA .input-group.mg-t-10').remove();
     $('#slWrapper2 option:eq(0)').prop("selected", true);
   } else if (qType == 'short-form-auto') {
     $('#privacyMCA').removeClass('d-block');
-    $('#privacySFA').addClass('d-block');
-    $('#prReqBtn').addClass('d-block');
+    $('#privacySFA, #prReqBtn').addClass('d-block');
     $('#qIp').addClass('d-none');
     $('#prReqBtn').removeClass('mg-l-10');
     $('#slWrapper2 option:eq(0)').prop("selected", true);
   } else if (qType == 's-multi-choice') {
-    $('#surveyMCA').addClass('d-block');
-    $('#suReqBtn').addClass('d-block');
+    $('#surveyMCA, #suReqBtn').addClass('d-block');
     $('#surveySFA').removeClass('d-block');
     $('#suReqBtn').addClass('mg-l-10');
   } else if (qType == 's-short-form') {
     $('#surveyMCA').removeClass('d-block');
-    $('#surveySFA').addClass('d-block');
-    $('#suReqBtn').addClass('d-block');
+    $('#surveySFA, #suReqBtn').addClass('d-block');
     $('#suReqBtn').addClass('mg-l-10');
     $('#surveyMCA .input-group.mg-t-10').remove();
     $('#slWrapper4 option:eq(0)').prop("selected", true);
   }
    else if (qType == '') {
-    $('#privacyMCA').removeClass('d-block');
-    $('#privacySFA').removeClass('d-block');
-    $('#prReqBtn').removeClass('d-block');
+    $('#privacyMCA, #privacySFA, #prReqBtn, #surveyMCA, #surveySFA, #suReqBtn').removeClass('d-block');
     $('#privacyMCA .input-group.mg-t-10').remove();
-    $('#surveyMCA').removeClass('d-block');
-    $('#surveySFA').removeClass('d-block');
-    $('#suReqBtn').removeClass('d-block');
     $('#surveyMCA .input-group.mg-t-10').remove();
   }
-  $('#prReqBtn').val(qType);
+  if (crtTab == 'privacy') {
+    $('#prReqBtn').val(qType);
+    $('#qIp, #mcIp0').val('');
+  } else if (crtTab == 'survey') {
+    $('#suReqBtn').val(qType);
+    $('#qIpS, #mcIpS0').val('');
+  }
+  
 });
 
 // $('select[name="sfaSelect"]').change(function(){
@@ -57,11 +55,16 @@ $('select[name=slWrapperSelect]').change(function (){
 // });
 
 var delCnt = 1;
+
 $('button[name=regBtn]').on('click', function(){
   var crtTab = $(this).val();
   var pushId = ['#privacyMCA', '#surveyMCA'];
   delCnt = $('#groupRelative>div').length;
+  var delCntS = $('#groupRelativeS>div').length;
+  
   var addHtml = '<div id=addNum'+delCnt+' class="input-group mg-t-10 reset"><div class="input-group-prepend"><span class="input-group-text"><i class="icon tx-16 lh-0 op-6"></i>'+(delCnt+1)+'</span></div><input id="mcIp'+delCnt+'" type="text" class="form-control" placeholder="선택 답안을 입력해 주세요."><button onclick="rmvThis(this)" name="minusIp" type="submit" class="btn btn-outline-light mg-l-10"><i class="icon ion-minus tx-14 "></i></button></div>';
+  var addHtmlS = '<div id=addNumS'+delCntS+' class="input-group mg-t-10 reset"><div class="input-group-prepend"><span class="input-group-text"><i class="icon tx-16 lh-0 op-6"></i>'+(delCntS+1)+'</span></div><input id="mcIpS'+delCntS+'" type="text" class="form-control" placeholder="선택 답안을 입력해 주세요."><button onclick="rmvThis(this)" name="minusIp" type="submit" class="btn btn-outline-light mg-l-10"><i class="icon ion-minus tx-14 "></i></button></div>';
+
 
   console.log('crtTab:',crtTab);
   if(crtTab == 'privacy') {
@@ -69,10 +72,11 @@ $('button[name=regBtn]').on('click', function(){
     $(pushId[0]+' #groupRelative').append(addHtml);
   } else if (crtTab == 'survey'){
     console.log('test survey');
-    $(pushId[1]+' #s-groupRelative').append(addHtml)
+    $(pushId[1]+' #groupRelativeS').append(addHtmlS)
   }
   ++delCnt;
 });
+
 
 $('input[name^=certi-chk]').change(function(){
   console.log('qwer');
@@ -112,7 +116,12 @@ function rmvThis (req){
   var delElId = req.parentNode.id;
   var primStr = delElId.replace(/[0-9]/g,"");
   var startNum = delElId.replace(/[^0-9]/g,"");
-  var reLeng = $('#groupRelative>div').length;
+  if (crtTab == 'privacy'){
+    var reLeng = $('#groupRelative>div').length;
+  } else if (crtTab == 'survey') {
+    var reLeng = $('#groupRelativeS>div').length;
+  }
+  
 
   req.parentNode.remove();
 
@@ -127,9 +136,15 @@ function rmvThis (req){
   $('[id^=addNum] .input-group-text').text(' ');
   
   for( var i=0; i<reLeng; i++ ){
-    console.log('i=',i)
-    $('#groupRelative>div:nth-child('+(i+1)+')').attr('id','addNum'+i);
-    $('#addNum'+i+' input').attr('id','mcIp'+i);
+    // console.log('i=',i)
+    if (crtTab == 'privacy'){
+      $('#groupRelative>div:nth-child('+(i+1)+')').attr('id','addNum'+i);
+      $('#addNum'+i+' input').attr('id','mcIp'+i);
+    } else if (crtTab == 'survey') {
+      $('#groupRelativeS>div:nth-child('+(i+1)+')').attr('id','addNumS'+i);
+      $('#addNumS'+i+' input').attr('id','mcIpS'+i);
+    }
+    
   }
 
 
@@ -142,48 +157,74 @@ function reqAdd(req) {
   var reqType = req.value;
   var reqArr = reqType.split(',');
 
-  var qNum = $('#collapseOne .card-block').length +1;
-  console.log('qNum: ', qNum);
-
-  // console.log('reqArr[0]: ',reqArr[0]);
-
+  
   var mcaTtl = $('#qIp').val();
-  var mcaPreviewA = '<div id="q'+reqCnt+'" class="card-block" name="'+qType+'"><div class="col-md-12 mg-y-10"><div class="list-group pd-b-20"><div class="list-group-item"><div class="media justify-content-center align-items-center"><div class="media-body"><h6 class="tx-inverse mg-0"><span class="pre-ttl-num">'+qNum+'.&nbsp;</span>'+mcaTtl+'</h6></div><i onclick="qRmvReq('+"'q"+(qNum - 1)+"'"+');" class="icon ion-ios-close-empty tx-28 mg-l-20 question-set"></i></div></div>';
+  var sfaTtl = $('#qIpS').val();
+  
+  console.log('reqType: ', reqType);
+
+  if (qType == 'multi-choice' || qType == 'short-form') {
+    var qNum = $('#collapseOne .card-block').length +1;
+    var mcaPreviewA = '<div id="q'+reqCnt+'" class="card-block" name="'+qType+'"><div class="col-md-12 mg-y-10"><div class="list-group pd-b-20"><div class="list-group-item"><div class="media justify-content-center align-items-center"><div class="media-body"><h6 class="tx-inverse mg-0"><span class="pre-ttl-num">'+qNum+'.&nbsp;</span>'+mcaTtl+'</h6></div><i onclick="qRmvReq('+"'q"+(qNum - 1)+"'"+');" class="icon ion-ios-close-empty tx-28 mg-l-20 question-set"></i></div></div>';
+  } else if (qType == 's-multi-choice' || qType == 's-short-form') {
+    var qNum = $('#collapseOneS .card-block').length +1;
+    var mcaPreviewA = '<div id="q'+reqCnt+'" class="card-block" name="'+qType+'"><div class="col-md-12 mg-y-10"><div class="list-group pd-b-20"><div class="list-group-item"><div class="media justify-content-center align-items-center"><div class="media-body"><h6 class="tx-inverse mg-0"><span class="pre-ttl-num">'+qNum+'.&nbsp;</span>'+sfaTtl+'</h6></div><i onclick="qRmvReq('+"'q"+(qNum - 1)+"'"+');" class="icon ion-ios-close-empty tx-28 mg-l-20 question-set"></i></div></div>';
+  }
+  
   var mcaPreviewB = '';
   var mcaPreviewC = '</div></div></div>';
-  var mcaVal = $('#mcIp0').val();
-  console.log('mcaTtl:', mcaTtl);
 
 
-  if (mcaTtl == '') {
+  if ($('#slWrapper select').val() !='' && mcaTtl == '') {
+    alert('질문을 입력해주세요');
+    return false;
+  }
+
+  if ($('#slWrapper3 select').val() !='' && sfaTtl == '') {
     alert('질문을 입력해주세요');
     return false;
   }
 
 
-  if(qType == 'multi-choice'){
-// 객관식 선택시 문항 개수 취합, 빈값 체크, 배치
+  if(qType == 'multi-choice' || qType == 's-multi-choice'){
 
-  var reqMCALength = $('[id^=mcIp]').length;
-  var reqMCAArr = new Array;
-  var reqSFType = $('select[name="sfaSelect"]').val();
-
-  reqMCAArr += $('[id^=mcIp]').val();
-
-  console.log('reqMCAArr: ', reqMCAArr);
-  console.log('reqMCALength: ', reqMCALength);
-
-  for (var i=0; i<reqMCALength; i++){
-    if($('#mcIp'+i).val() == ''){
-      alert('선택 답안을 입력해주세요');
-      return false;
+    switch (qType) {
+      case 'multi-choice':
+        var reqMCALength = $('#groupRelative input').length;
+        for (var i=0; i<reqMCALength; i++){
+          if($('#mcIp'+i).val() == ''){
+            alert('선택 답안을 입력해주세요');
+            return false;
+          }
+          mcaPreviewB += '<div class="list-group-item pd-y-6"><div class="media"><div class="media-body"><label class="rdiobox"><input name="question-radio'+(reqCnt+1)+'" value="'+(i+1)+'" type="radio"><span>'+$('#mcIp'+i).val()+'</span></label></div></div></div>';
+        }
+        break;
+      case 's-multi-choice':
+        var reqMCALength = $('#groupRelativeS input').length;
+        for (var i=0; i<reqMCALength; i++){
+          if($('#mcIpS'+i).val() == ''){
+            alert('선택 답안을 입력해주세요');
+            return false;
+          }
+          mcaPreviewB += '<div class="list-group-item pd-y-6"><div class="media"><div class="media-body"><label class="rdiobox"><input name="question-radio'+(reqCnt+1)+'" value="'+(i+1)+'" type="radio"><span>'+$('#mcIpS'+i).val()+'</span></label></div></div></div>';
+        }
+        break;
     }
-    mcaPreviewB += '<div class="list-group-item pd-y-6"><div class="media"><div class="media-body"><label class="rdiobox"><input name="question-radio'+(reqCnt+1)+'" value="'+(i+1)+'" type="radio"><span>'+$('#mcIp'+i).val()+'</span></label></div></div></div>';
-  }
+//  console.log('reqMCAArr: ', reqMCAArr);
+//  console.log('reqMCALength: ', reqMCALength);
 
-  } else if (qType == 'short-form') {
-    var sfaType = $('select[name="sfaSelect"]').val();
 
+  } else if (qType == 'short-form' || qType == 's-short-form') {
+    switch(qType){
+      case 'short-form':
+        var sfaType = $('select[name="sfaSelect"]').val();
+        break;
+      case 's-short-form':
+        var sfaType = $('select[name="sfaSelectS"]').val();
+        break;
+    }
+    
+    console.log('sfaType: ',sfaType);
     //  sfaPhone   sfaAddr  sfaJumin  sfaEmail  sfaDate sfaFile sfaTxt
 
     switch(sfaType){
@@ -194,7 +235,7 @@ function reqAdd(req) {
         mcaPreviewB += '<div name='+sfaType+' class="list-group-item pd-y-14"><div class="media"><div class="media-body"><div class="input-group"><div class="input-group-prepend"><div class="input-group-text"><i class="fa fa-phone tx-16 lh-0 op-6"></i></div></div><input name="sf-phone'+(reqCnt+1)+'" id="phoneMask" type="text" class="form-control" placeholder="010-1234-1234" value=""></div></div></div></div>';
         break;
       case 'sfaAddr':
-        mcaPreviewB += '';
+        mcaPreviewB += '<div name='+sfaType+' class="list-group-item pd-y-14"><div class="media"><div class="media-body"><div class="input-group"><input name="sf-txt'+(reqCnt+1)+'" id="" class="form-control" type="text" placeholder="주소를 입력해 주세요" value=""></div></div></div></div>';
         break;
       case 'sfaJumin':
         mcaPreviewB += '<div name='+sfaType+' class="list-group-item pd-y-14"><div class="media"><div class="media-body"><div class="input-group"><input id="" class="form-control mg-x-10" name="sf-juminFt'+(reqCnt+1)+'" maxlength="6" oninput="numMax(this);" type="number" value="" placeholder="890123"><input id="" class="form-control mg-x-10" name="sf-juminBk'+(reqCnt+1)+'" maxlength="7" oninput="numMax(this);" type="password" value="" placeholder="1234567"></div></div></div></div>';
@@ -213,12 +254,17 @@ function reqAdd(req) {
         break;
     }
   }
-
-
-
-  console.log(mcaPreviewB);
-  $('#preview').css('display','block');
-  $('#collapseOne').append(mcaPreviewA+mcaPreviewB+mcaPreviewC);
+  console.log("mcaPreviewA= ", mcaPreviewA);
+  console.log("mcaPreviewB= ", mcaPreviewB);
+  if (qType == 'multi-choice' || qType == 'short-form') {
+    $('#preview').css('display','block');
+    $('#collapseOne').append(mcaPreviewA+mcaPreviewB+mcaPreviewC);
+  } else if (qType == 's-multi-choice' || qType == 's-short-form') {
+    $('#previewS').css('display','block');
+    $('#collapseOneS').append(mcaPreviewA+mcaPreviewB+mcaPreviewC);
+  }
+  
+  
 
   if(qType == 'short-form' && sfaType == 'sfaPhone' ){
     $('#phoneMask').mask('999-9999-9999');
@@ -228,13 +274,10 @@ function reqAdd(req) {
 
 
   //question reset
-  $('select[name=slWrapperSelect]').val('');
-  $('#qIp').val('');
-  $('#mcIp0').val('');
-  $('#privacyMCA').removeClass('d-block');
-  $('#prReqBtn').addClass('d-none');
-  $('#privacySFA').removeClass('d-block');
-  $('#groupRelative .reset').remove();
+  $('select[name=slWrapperSelect], select[name=sfaSelect], #qIp, #qIpS, #mcIp0, #mcIpS0').val('');
+  $('#privacyMCA, #privacySFA, #surveyMCA, #surveySFA').removeClass('d-block');
+  $('#prReqBtn, #suReqBtn').addClass('d-none');
+  $('#groupRelative .reset, #groupRelativeS .reset').remove();
   
 
   ++reqCnt;
@@ -295,12 +338,16 @@ function chgIconArrow(){
 
 
 function saveEvt() {
-  console.log('card-block: ', $('#collapseOne .card-block').length);
   
-  if ($('#collapseOne .card-block').length == 0){
+  if ( (qType == '' || qType == 'multi-choice' || qType == 'short-form') && $('#collapseOne .card-block').length == 0){
+    alert('적용된 문항이 없습니다.');
+    return false;
+  } else if ( (qType == '' || qType == 's-multi-choice' || qType == 's-short-form') && $('#collapseOneS .card-block').length == 0){
     alert('적용된 문항이 없습니다.');
     return false;
   }
+  
+
   $('body').prepend('<div class="sk-three-bounce"><div class="sk-child sk-bounce1"></div><div class="sk-child sk-bounce2"></div><div class="sk-child sk-bounce3"></div></div>');
   $('body').append('<div class="modal-backdrop fade show"></div>');
   $('#saveEvtMd').removeClass('show');
@@ -327,10 +374,20 @@ function qRmvReqDel() {
 
   // .card-block id reset
   $('.card-block').attr('id','');
-  var reLeng = $('#collapseOne>div').length;
+  if(crtTab == 'privacy') {
+    var reLeng = $('#collapseOne>div').length;
+  } else if (crtTab == 'survey'){
+    var reLeng = $('#collapseOneS>div').length;
+  }
+  
   for( var i=0; i<reLeng; i++ ){
     console.log('i=',i)
-    $('#collapseOne>div:nth-child('+(i+1)+')').attr('id','q'+i);
+    if(crtTab == 'privacy') {
+      $('#collapseOne>div:nth-child('+(i+1)+')').attr('id','q'+i);
+    } else if (crtTab == 'survey'){
+      $('#collapseOneS>div:nth-child('+(i+1)+')').attr('id','q'+i);
+    }
+    
     $('#q'+i+' i.icon').attr('onclick','qRmvReq("q'+i+'")');
     $('#q'+i+' span.pre-ttl-num').text((i+1)+'. ');
   }
@@ -342,15 +399,14 @@ function qRmvReqDel() {
 
 // reset question case change tab
 function resetEvt(e) {
+  crtTab = $(e).attr('value');
+  console.log('crtTab:',  crtTab);
   console.log('this class: ', $(e).attr('class'));
   if(!$(e).hasClass('active')){
-    $('#preview').css('display','none');
-    $('#collapseOne div').remove();
-    $('#privacySFA').removeClass('d-block');
-    $('#privacyMCA').removeClass('d-block');
-    $('select[name=slWrapperSelect]').val('');
-    $('#qIp').val('');
-    $('#qIpS').val('');
+    $('#preview, #previewS').css('display','none');
+    $('#collapseOne div, #collapseOneS div, #groupRelative .reset, #groupRelativeS .reset, #groupRelativeS .reset').remove();
+    $('#privacySFA, #surveySFA, #privacyMCA, #surveyMCA').removeClass('d-block');
+    $('select[name=slWrapperSelect], select[name=sfaSelect], #qIp, #qIpS, #mcIp0, #mcIpS0').val('');
   }
 }
 
